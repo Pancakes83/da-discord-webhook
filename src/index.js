e7bc0f8231742f03348179c6ff5a522a33258502
Pancakes83 <46944208+Pancakes83@users.noreply.github.com>
2019-08-05 00:18:41 -0500

    Update index.js
    first pass

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
 import { Client } from 'darkages';
 import Webhook from './discord';
 
-const { USERNAME, PASSWORD, WEBHOOK } = process.env;
+const { USERNAME, PASSWORD, WEBHOOK, CLASS1, CLASS2 } = process.env;
 const client = new Client(USERNAME, PASSWORD);
 const webhook = new Webhook(WEBHOOK);
+const webhook2 = new Webhook(WEBHOOK2);
+const webhook3 = new Webhook(WEBHOOK3);
+const webhook4 = new Webhook(WEBHOOK4);
+const webhook5 = new Webhook(WEBHOOK5);
+
+
 
 client.events.on(0x0A, packet => {
   const type = packet.readByte();
   const message = packet.readString16();
 
   if (type === 5) {
-    const battleAnnounced = (
-      message.startsWith('Host') ||
-      message.startsWith('The Balanced Arena')
+    const gmAnnounced = (
+      message.startsWith('[Error]') ||
+      message.startsWith('[Trial]') ||
+      message.startsWith('[Ishikawa]')
     );
+    const classAnnounced = (
+      message.includes('will be teaching') &&
+      message.includes('at the Mileth College.')
+      );
+    const entryPosted = (
+      message.includes('will be posting an entry in the Contest Hall for review.')
+      );
 
-    if (battleAnnounced) {
-      webhook.send(message);
+    if (gmAnnounced) {
+      webhook3.send(message);
     }
-  }
+    else if (classAnnounced) {
+      webhook2.send(CLASS1 + message) &&
+      webhook5.send(CLASS2 + message);
+                    }
+     else if (entryPosted) {
+        webhook4.send("@everyone" + message)
+      }
+    else {
+      webhook.send(message)
+    }
 });
 
 client.connect();
